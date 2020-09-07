34cc0667298829210e394399bdd92d350590732c
juner <juner@juner.net>
2020-09-07 10:20:46 +0900

    It does not fall into my mind, but it has been resolved.
    -> Hi Jun,
    
    ScrollSettings and PageSettings properties were initialized with default values in the SfDiagram's Constructor. While you using SfDiagram as DataTemplate, the property which is already initialized doesn't gets updated for xaml values.
    
    So,we have solved this issue by creating custom class for SfDiagram and updated the properties in the constructor of the custom class. Then use that custom Diagram in the template to achieve your requirement. Please refer the code changes as below.
    
    Code changes:
    
    ```
    <local:CustomDiagram Background="Yellow" Height="120" Width="120" Tool="None" HorizontalRuler="{x:Null}" VerticalRuler="{x:Null}">
                            <local:CustomDiagram.Nodes>
                                <syncfusion:NodeCollection>
                                    <syncfusion:NodeViewModel UnitHeight="50" UnitWidth="50" OffsetX="30" OffsetY="30" />
                                </syncfusion:NodeCollection>
                            </local:CustomDiagram.Nodes>
    </local:CustomDiagram>
    
    public class CustomDiagram : SfDiagram
        {
            public CustomDiagram() : base()
            {
                this.PageSettings = new PageSettings() { PageWidth = 100, PageHeight = 100, PageBackground = new SolidColorBrush(Colors.Transparent) };
                this.ScrollSettings = new ScrollSettings() { ScrollLimit = ScrollLimit.Diagram, DragLimit = ScrollLimit.Diagram, AutoScrollLimit = ScrollLimit.Diagram };
            }
        }
    ```
    
    Regards, \
    Ranjitha A.
    
    https://www.syncfusion.com/forums/157491/sfdiagram-bug-i-cant-set-pagebackground-of-pagesettings-to-transparent-when-i-put-sfdiagram

---
+++ ListInSfDiagramSample/UserControls/SfDiagram.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+using Syncfusion.UI.Xaml.Diagram;
+
+namespace ListInSfDiagramSample.UserControls
+{
+    public class SfDiagram : Syncfusion.UI.Xaml.Diagram.SfDiagram
+    {
+        public SfDiagram() : base()
+        {
+            PageSettings = new PageSettings
+            {
+                PageBackground = Brushes.Transparent,
+                PageHeight = 100,
+                PageWidth = 100,
+            };
+            ScrollSettings = new ScrollSettings
+            {
+                ScrollLimit = ScrollLimit.Diagram,
+                AutoScrollLimit = ScrollLimit.Diagram,
+                DragLimit = ScrollLimit.Diagram,
+            };
+        }
+    }
+}
